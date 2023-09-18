a0b3b2946f6366139b9ab0ac88b1a25139aeabb9
ponyjackal <pony1125@outlook.com>
2023-09-18 06:00:44 -0700

    feat: add account abstraction wallet

---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+import { DEFAULT_ENTRYPOINT_ADDRESS } from "@biconomy/account";
+import { IBundler, Bundler } from "@biconomy/bundler";
+import { ChainId } from "@biconomy/core-types";
+import { BiconomyPaymaster, IPaymaster } from "@biconomy/paymaster";
+
+export const bundler: IBundler = new Bundler({
+  bundlerUrl:
+    "https://bundler.biconomy.io/api/v2/80001/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44",
+  chainId: ChainId.POLYGON_MUMBAI,
+  entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
+});
+
+export const paymaster: IPaymaster = new BiconomyPaymaster({
+  paymasterUrl:
+    "https://paymaster.biconomy.io/api/v1/5/P2Fom_4H2.9225c73e-9e2b-4564-a45a-9c734c53388d",
+});
